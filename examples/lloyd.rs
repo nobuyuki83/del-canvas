900e5d162fa9dd94a6b34efe15f52315ae5db54b
Nobuyuki <n.umetani@gmail.com>
2024-05-12 22:58:00 +0900

    refactoring

--- examples/lloyd.rs
+++ examples/lloyd.rs
@@ -13,29 +13,28 @@ fn main() {
     let mut site2xy
         = del_msh::sampling::poisson_disk_sampling_from_polyloop2(
         &vtxl2xy, 0.08, 100);
-    let vtxl2xy
-        = del_msh::vtx2xyz::to_array_of_nalgebra_vector::<f32, 2>(&vtxl2xy);
     for _iter in 0..50 {
         canvas.clear(0);
-        let (site2vtxc2xy, _site2vtxc2info)
-            = del_msh::volonoi2::volonoi_cells(&vtxl2xy, &site2xy);
-        let mut site2xy_new = site2xy.clone();
-        for i_site in 0..site2xy.len() {
+        let (site2vtxc2xy, _site2vtxc2info) = {
+            del_msh::voronoi2::voronoi_cells(&vtxl2xy, &site2xy)
+        };
+        let mut site2xy_new = del_msh::vtx2xyz::to_array_of_nalgebra_vector(&site2xy);
+        for i_site in 0..site2xy.len() / 2 {
             let vtxc2xy = &site2vtxc2xy[i_site];
-            let cell = del_msh::vtx2xyz::from_array_of_nalgebra(&vtxc2xy);
-            let p = del_msh::polyloop::cog::<f32, 2>(&cell);
-            site2xy_new[i_site] = p;
+            let vtxc2xy = del_msh::vtx2xyz::from_array_of_nalgebra(&vtxc2xy);
+            let p = del_msh::polyloop2::cog_area(&vtxc2xy);
+            site2xy_new[i_site] = p.into();
             canvas.paint_polyloop(
-                &cell, &transform_to_scr,
+                &vtxc2xy, &transform_to_scr,
                 1., 1);
             canvas.paint_point(
-                site2xy[i_site].x, site2xy[i_site].y, &transform_to_scr,
+                site2xy[i_site*2+0], site2xy[i_site*2+1], &transform_to_scr,
                 3.0, 1);
             canvas.paint_point(
-                p.x, p.y, &transform_to_scr,
+                p[0], p[1], &transform_to_scr,
                 3.0, 2);
         }
         canvas.write();
-        site2xy = site2xy_new;
+        site2xy = del_msh::vtx2xyz::from_array_of_nalgebra(&site2xy_new);
     }
 }
